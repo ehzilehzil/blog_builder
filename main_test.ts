8839bd1aae481ef112c5c70efa88a460a04d7640
ehzhilehzil <ehzilehzil@outlook.com>
2025-08-13 21:02:47 +0900

    renderer

--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,22 @@
-import * as renderer from "./renderer.ts";
+import {renderer} from "./renderer.ts";
 
 {
-    const result = await renderer.UnoCSS(`<h1 class="w-full h-full">안녕</h1>`);
-    console.log(result);
+    const md = "# 안녕\n```js\nconsole.log('안녕');\n```";
+    const res = renderer.markdown(md);
+    console.log(res);
+}
+
+{
+    const css = "div(class='bg-blue-500')\n  p(class='text-xl') 안녕";
+    const res = await renderer.unocss(css);
+    console.log(res);
+
+    const res2 = renderer.pug(css, {});
+    console.log(res2);
+}
+
+{
+    const md = "---\nlayout: 'page'\n---\n# 안녕";
+    const res = renderer.matter(md);
+    console.log(res.data, renderer.markdown(res.content));
 }
--- renderer.ts
+++ renderer.ts
@@ -1,20 +1,42 @@
 import {createGenerator, transformerDirectives, presetIcons, presetUno} from "unocss";
+// import extractorPug from "@unocss/extractor-pug";
+import pug from "pug";
 import MarkdownIt from "markdown-it";
 import hljs from "highlight.js";
+import matter from "gray-matter";
 
-export async function unocss(src: string): Promise<string> {
+async function unocss(src: string): Promise<string> {
     const uno = await createGenerator({
         presets: [presetIcons(), presetUno()],
         transformers: [transformerDirectives()],
         preflights: [{
             getCSS: async ({theme}) => await Deno.readTextFile("./_assets/reset-css.css"),
         }],
+        // extractors: [
+        //     extractorPug(),
+        // ],
     });
     return (await uno.generate(src)).css;
 }
 
-export const markdownit = new MarkdownIt({
-    Highlight: (code, lang) => {
-        lang = hljs.getLanguage(lang) ? lang : "plaintext";
+const md = new MarkdownIt({
+    highlight: (code, lang) => {
+        const language = hljs.getLanguage(lang)?.name ?? "plaintext";
+        
+        try {
+            const res = `<pre class="hljs language-${lang}"><code>` +
+                        hljs.highlight(code, {language}).value +
+                        `</code></pre>`;
+            return res;
+        } catch(_e) {}
+        
+        return "";
     },
 });
+
+export const renderer = {
+    markdown(str: string) {return md.render(str);},
+    matter(str: string) {return matter(str);},
+    async unocss(str: string) {return await unocss(str);},
+    pug(str: string, obj: pug.LocalsObject) {return pug.render(str, obj);},
+};
